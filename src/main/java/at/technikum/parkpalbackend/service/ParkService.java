6e7eaf4de70917a218681fde20c5907f7df15a49
Lucaww23e013 <ww23e013@technikum-wien.at>
2024-04-19 13:33:48 +0200

    Updated the attributes (parkName -> name...)

--- src/main/java/at/technikum/parkpalbackend/service/ParkService.java
+++ src/main/java/at/technikum/parkpalbackend/service/ParkService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ParkService {
+public class  ParkService {
     private final ParkRepository parkRepository;
 
     public ParkService(ParkRepository parkRepository) {
