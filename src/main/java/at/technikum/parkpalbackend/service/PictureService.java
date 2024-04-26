e9b70db93d2a9fa582c2edfef6554034192236d1
zirbitz <ww23e018@technikum-wien.at>
2024-04-26 11:01:33 +0200

    Ppp:23-Test for VideoService.java. added; new Elements for Videos in TestFixtures.java added fixes in VideoSer .java & VideoService.java & VideoRepository.java Methods

--- src/main/java/at/technikum/parkpalbackend/service/PictureService.java
+++ src/main/java/at/technikum/parkpalbackend/service/PictureService.java
@@ -6,7 +6,6 @@ import at.technikum.parkpalbackend.model.User;
 import at.technikum.parkpalbackend.persistence.PictureRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -51,9 +50,5 @@ public class PictureService {
                 .orElseThrow(EntityNotFoundException::new);
         pictureRepository.delete(picture);
         return null;
-    }
-
-    public LocalDateTime getUploadDate() { //change with FileController
-        return LocalDateTime.now();
     }
 }
