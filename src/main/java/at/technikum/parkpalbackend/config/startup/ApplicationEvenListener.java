8f4a5d39e86dc058550d08d7dbff3f996b1459b9
Osama <osama.madani9@gmail.com>
2024-10-30 16:24:54 +0100

    make 200-make-test-component-test-not-integration-test

--- src/main/java/at/technikum/parkpalbackend/config/startup/ApplicationEvenListener.java
+++ src/main/java/at/technikum/parkpalbackend/config/startup/ApplicationEvenListener.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.stereotype.Component;
 
@@ -14,16 +15,27 @@ import javax.sql.DataSource;
 @AllArgsConstructor
 public class ApplicationEvenListener {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
+    private final Environment environment;
 
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationReadyEvent() {
-        String activeProfile = System.getProperty("spring.profiles.active");
-        if (activeProfile == null || !activeProfile.equalsIgnoreCase("test")) {
+        String[] activeProfiles = environment.getActiveProfiles();
+        log.info("Active Profiles: {}", (Object) activeProfiles);
+        boolean isTestProfileActive = false;
+        for (String profile : activeProfiles) {
+            if ("test".equalsIgnoreCase(profile)) {
+                isTestProfileActive = true;
+                break;
+            }
+        }
+        if (!isTestProfileActive) {
             ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
             populator.addScript(new org.springframework.core.io.ClassPathResource("data.sql"));
             populator.execute(dataSource);
             log.info("Database populated.");
+        } else {
+            log.info("Skipping database population for test profile.");
         }
     }
 }
